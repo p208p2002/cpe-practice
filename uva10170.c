e32579b151b78f75f0a7b1f0e1f3c115e39d3836
Philip <p208p2002@gmail.com>
2018-06-11 02:52:53 +0800

    Update uva10170.c

--- uva10170.c
+++ uva10170.c
@@ -2,28 +2,25 @@
 #include <stdio.h>
 
 int main(){
-	unsigned long long int S,D,input,output,i,j,onDay;	
+	unsigned long long int S,D,input,output,onDay;	
 	int isS = 1;
-	while(scanf("%d",&input)!=EOF){
+	while(scanf("%lld",&input)!=EOF){
 		//get input		
 		if(isS)
-			S = (int)input;		
+			S = (long long int)input;		
 		else
-			D = (int)input;		
+			D = (long long int)input;		
 		
 		//data ready
-		if(!isS){
-			onDay=1;
-			while(onDay <= D){
-				for(i=0;i<S;i++){
-					//printf("%d",S);
-					output = S;
-					onDay++;
-				}
+		if(!isS){			
+			onDay = 0;
+			while(1){
+				onDay = onDay + S;				
+				if(onDay >= D)
+					break;
 				S++;
 			}
-			printf("%d\n",output);
-			//printf("\n");
+			printf("%d\n",S);
 		}
 		
 		//
